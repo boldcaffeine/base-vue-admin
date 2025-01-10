bbfb79ad83fa2ebf279ac76afe6d7d8ed22e5f4a
早起喝燕麦 <zhuowangyi@163.com>
2025-01-10 10:30:39 +0800

    store

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,3 +1,29 @@
 <template>
-  <div>我是dashboard</div>
+  <div>
+    我是dashboard
+    <div>{{ count }}</div>
+    <el-button @click="increment">count</el-button>
+  </div>
 </template>
+<script>
+import { mapState } from "vuex";
+export default {
+  data: () => {
+    return {
+      message: "3333",
+    };
+  },
+  computed: mapState({
+    // 箭头函数可使代码更简练
+    count: (state) => state.count,
+    // 传字符串参数 'count' 等同于 `state => state.count`
+    countAlias: "count",
+  }),
+  methods: {
+    increment() {
+      this.$store.commit("increment");
+      console.log(this.$store.state.count);
+    },
+  },
+};
+</script>
