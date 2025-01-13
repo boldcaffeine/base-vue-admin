bdff3da7d4ad3ae4b7f17e0d717b6611e3880547
早起喝燕麦 <zhuowangyi@163.com>
2025-01-13 19:55:24 +0800

    style

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import Vue from "vue";
 import "normalize.css";
 import ElementUI from "element-ui";
 import "element-ui/lib/theme-chalk/index.css";
-import "./styles/element-variables.scss";
 
 import "@/globalStyles/index.scss"; // global css
 
