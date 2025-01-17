b00ccec60ecbd050393b728c593d8d18c23d60e1
早起喝燕麦 <zhuowangyi@163.com>
2025-01-17 17:37:04 +0800

    删除测试数据

--- src/layout/components/Sidebar/index.vue
+++ src/layout/components/Sidebar/index.vue
@@ -28,115 +28,6 @@ import RecursiveMenu from "./RecursiveMenu.vue";
 
 export default {
   components: { RecursiveMenu, Logo },
-  data: () => {
-    return {
-      menuData: [
-        {
-          index: "1",
-          title: "导航一",
-          icon: "el-icon-location",
-          children: [
-            {
-              index: "1-1",
-              title: "选项1",
-              children: [
-                { index: "1-1-1", title: "选项1-1" },
-                { index: "1-1-2", title: "选项1-2" },
-              ],
-            },
-            {
-              index: "1-2",
-              title: "选项2",
-              children: [
-                { index: "1-2-1", title: "选项2-1" },
-                { index: "1-2-2", title: "选项2-2" },
-              ],
-            },
-            { index: "1-3", title: "选项3" },
-            {
-              index: "1-4",
-              title: "选项4",
-              children: [
-                { index: "1-4-1", title: "选项4-1" },
-                { index: "1-4-2", title: "选项4-2" },
-                {
-                  index: "1-4-3",
-                  title: "选项4-3",
-                  children: [
-                    { index: "1-4-3-1", title: "选项4-3-1" },
-                    { index: "1-4-3-2", title: "选项4-3-2" },
-                  ],
-                },
-              ],
-            },
-          ],
-        },
-        {
-          index: "2",
-          title: "导航二",
-          icon: "el-icon-menu",
-          children: [
-            {
-              index: "2-1",
-              title: "选项1",
-              children: [
-                { index: "2-1-1", title: "选项1-1" },
-                { index: "2-1-2", title: "选项1-2" },
-              ],
-            },
-            {
-              index: "2-2",
-              title: "选项2",
-              children: [
-                { index: "2-2-1", title: "选项2-1" },
-                { index: "2-2-2", title: "选项2-2" },
-              ],
-            },
-          ],
-        },
-        {
-          index: "3",
-          title: "导航三",
-          icon: "el-icon-document",
-          disabled: true,
-          children: [
-            {
-              index: "3-1",
-              title: "选项1",
-              children: [
-                { index: "3-1-1", title: "选项1-1" },
-                { index: "3-1-2", title: "选项1-2" },
-              ],
-            },
-          ],
-        },
-        {
-          index: "4",
-          title: "导航四",
-          icon: "el-icon-setting",
-          disabled: false,
-          children: [
-            {
-              index: "4-1",
-              title: "选项1",
-              children: [
-                { index: "4-1-1", title: "选项1-1" },
-                { index: "4-1-2", title: "选项1-2" },
-              ],
-            },
-            {
-              index: "4-2",
-              title: "选项2",
-              children: [
-                { index: "4-2-1", title: "选项2-1" },
-                { index: "4-2-2", title: "选项2-2" },
-              ],
-            },
-          ],
-        },
-      ],
-    };
-  },
   computed: {
     ...mapGetters(["sidebar", "permission_routes"]),
     activeMenu() {
@@ -158,14 +49,6 @@ export default {
   },
   mounted() {
     console.log(this.permission_routes, "permission_routes");
-  },
-  methods: {
-    handleOpen() {
-      // handle open logic
-    },
-    handleClose() {
-      // handle close logic
-    },
   },
 };
 </script>
