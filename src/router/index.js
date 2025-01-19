e5590bf2fc10ffdf5be4c3f553d0f4e8237336b8
zhuowenxuan <zhuowenxuan@youfish.com>
2025-01-19 16:07:28 +0800

    driver.js

--- src/router/index.js
+++ src/router/index.js
@@ -66,6 +66,19 @@ export const constantRoutes = [
     ]
   },
   {
+    path: '/guide',
+    component: Layout,
+    redirect: '/guide/index',
+    children: [
+      {
+        path: 'index',
+        component: () => import('@/views/guide/index.vue'),
+        name: 'Guide',
+        meta: { title: 'Guide', icon: 'guide', noCache: true }
+      }
+    ]
+  },
+  {
     path: '/redirect',
     component: Layout,
     hidden: true,
---
+++ src/views/guide/index.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="app-container">
+    <div class="info">
+      The guide page is useful for some people who entered the project for the
+      first time. You can briefly introduce the features of the project. Demo is
+      based on
+      <a href="https://github.com/kamranahmedse/driver.js" target="_blank"
+        >driver.js.</a
+      >
+    </div>
+    <el-button
+      icon="el-icon-question"
+      type="primary"
+      @click.prevent.stop="guide"
+    >
+      Show Guide
+    </el-button>
+  </div>
+</template>
+
+<script>
+import { driver } from "driver.js";
+import "driver.js/dist/driver.css";
+import steps from "./steps";
+
+export default {
+  name: "Guide",
+  data() {
+    return {
+      steps: steps,
+      driverObj: {},
+    };
+  },
+  mounted() {
+    this.handleInit();
+  },
+  methods: {
+    handleInit() {
+      this.driverObj = driver({
+        showProgress: true,
+        steps: this.steps,
+      });
+    },
+    guide() {
+      this.driverObj.drive();
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.app-container {
+  padding: 16px;
+  .info {
+    background: #eef1f6;
+    padding: 8px 24px;
+    margin-bottom: 20px;
+    border-radius: 2px;
+    display: block;
+    line-height: 32px;
+    font-size: 16px;
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+      Ubuntu, Cantarell, "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif;
+    color: #2c3e50;
+    -webkit-font-smoothing: antialiased;
+    a {
+      color: #337ab7;
+      cursor: pointer;
+    }
+  }
+}
+</style>
