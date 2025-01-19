074542bec8219a061c632549b5fabd44599da402
zhuowenxuan <zhuowenxuan@youfish.com>
2025-01-19 11:21:24 +0800

    去除升级来的警告

--- src/views/dashboard/admin/index.vue
+++ src/views/dashboard/admin/index.vue
@@ -1,59 +1,74 @@
 <template>
-<div class="dashboard-editor-container">
-  <github-corner class="github-corner" />
-  <panel-group @handleSetLineChartData="handleSetLineChartData" />
-  <el-row style="background:#fff;padding:16px 16px 0;margin-bottom:32px;">
-   
+  <div class="dashboard-editor-container">
+    <github-corner class="github-corner" />
+    <panel-group @handleSetLineChartData="handleSetLineChartData" />
+    <el-row style="background: #fff; padding: 16px 16px 0; margin-bottom: 32px">
       <line-chart :chart-data="lineChartData" />
     </el-row>
-</div>
+    <el-row :gutter="32">
+      <el-col :xs="24" :sm="24" :lg="8">
+        <div class="chart-wrapper">
+          <raddar-chart />
+        </div>
+      </el-col>
+      <el-col :xs="24" :sm="24" :lg="8">
+        <div class="chart-wrapper">
+      
+        </div>
+      </el-col>
+      <el-col :xs="24" :sm="24" :lg="8">
+        <div class="chart-wrapper">
+      
+        </div>
+      </el-col>
+    </el-row>
+  </div>
 </template>
 
 <script>
-
-import GithubCorner from '@/components/GithubCorner';
-import PanelGroup from './components/PanelGroup'
-import LineChart from './components/LineChart'
+import GithubCorner from "@/components/GithubCorner";
+import PanelGroup from "./components/PanelGroup";
+import LineChart from "./components/LineChart";
+import RaddarChart from './components/RaddarChart'
 const lineChartData = {
   newVisitis: {
     expectedData: [100, 120, 161, 134, 105, 160, 165],
-    actualData: [120, 82, 91, 154, 162, 140, 145]
+    actualData: [120, 82, 91, 154, 162, 140, 145],
   },
   messages: {
     expectedData: [200, 192, 120, 144, 160, 130, 140],
-    actualData: [180, 160, 151, 106, 145, 150, 130]
+    actualData: [180, 160, 151, 106, 145, 150, 130],
   },
   purchases: {
     expectedData: [80, 100, 121, 104, 105, 90, 100],
-    actualData: [120, 90, 100, 138, 142, 130, 130]
+    actualData: [120, 90, 100, 138, 142, 130, 130],
   },
   shoppings: {
     expectedData: [130, 140, 141, 142, 145, 150, 160],
-    actualData: [120, 82, 91, 154, 162, 140, 130]
-  }
-}
-
+    actualData: [120, 82, 91, 154, 162, 140, 130],
+  },
+};
 
 export default {
-  name: 'DashboardAdmin',
+  name: "DashboardAdmin",
   components: {
     GithubCorner,
     PanelGroup,
-    LineChart
+    LineChart,
+    RaddarChart
   },
   data() {
     return {
-      lineChartData: lineChartData.newVisitis
-    }
+      lineChartData: lineChartData.newVisitis,
+    };
   },
   methods: {
     handleSetLineChartData(type) {
-      this.lineChartData = lineChartData[type]
-    }
-  }
-}
+      this.lineChartData = lineChartData[type];
+    },
+  },
+};
 </script>
-
 
 <style lang="scss" scoped>
 .dashboard-editor-container {
@@ -75,7 +90,7 @@ export default {
   }
 }
 
-@media (max-width:1024px) {
+@media (max-width: 1024px) {
   .chart-wrapper {
     padding: 8px;
   }
