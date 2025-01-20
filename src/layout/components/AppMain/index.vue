42646bf72c9964a3cd51eec2a78b4e04c219b9ca
早起喝燕麦 <zhuowangyi@163.com>
2025-01-20 20:26:24 +0800

    动画

--- src/layout/components/AppMain/index.vue
+++ src/layout/components/AppMain/index.vue
@@ -23,6 +23,22 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+
+/* fade-transform */
+.fade-transform-leave-active,
+.fade-transform-enter-active {
+  transition: all .5s;
+}
+
+.fade-transform-enter {
+  opacity: 0;
+  transform: translateX(-30px);
+}
+
+.fade-transform-leave-to {
+  opacity: 0;
+  transform: translateX(30px);
+}
 .app-main {
   /* 50= navbar  50  */
   min-height: calc(100vh - 50px);
